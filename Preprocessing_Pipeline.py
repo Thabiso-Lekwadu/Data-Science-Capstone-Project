bd5631d65292f2eafaf45327abd5aa21b585e0bf
Amare <amare@example.com>
2026-02-11 10:22:57 +0200

    initialize project structure with main script and configuration files

---
+++ Preprocessing_Pipeline.py
@@ -0,0 +1,12 @@
+class pipeline:
+    def __init__(self, df):
+        self.df = df
+
+    def preprocess(df):
+        pass
+
+    def impute_missing_values(df):
+        pass
+
+    def
+    def encode_categorical_variables(df):
